84384ad360f7c4c2dae24a2b4fce45f173a52787
Wilson Lasso <wilago1979@gmail.com>
2022-06-29 16:26:59 -0500

    Creacion del servicio auth y  guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MENU_ROUTES } from './components/menu/menu.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent  },
+  //{path:'**', pathMatch:'full', redirectTo:'home'  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path:'login', component : LoginComponent  },
+  {path:"menu", 
+      component : MenuComponent,
+      children: MENU_ROUTES
+  },
+{path:'**', pathMatch:'full', redirectTo:'login'  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
